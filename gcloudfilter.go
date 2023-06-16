18a7c9095f0a1a5a50ec2e300d433714117eeed5
kosmas.valianos <kosmas.valianos@appgate.com>
2023-06-16 11:19:22 +0200

    Better print

--- gcloudfilter.go
+++ gcloudfilter.go
@@ -12,32 +12,32 @@ type Filter struct {
 	LogicalOperators []*LogicalOperators `| @@*`
 }
 
+func (f *Filter) String() string {
+	var sb strings.Builder
+	sb.Grow(256)
+	sb.WriteString("Terms:")
+	for _, p := range f.Terms {
+		sb.WriteString(fmt.Sprintf("\n\tTerm: %v", p))
+	}
+	return sb.String()
+}
+
 type LogicalOperators struct {
 	Operator string `@("AND" | "OR" | "NOT")`
 }
 
 type Term struct {
 	Key      string `@Ident (@"." @Ident)*`
-	Operator string `@(":" | "=" | "<=")`
+	Operator string `@(":" | "=" | "!=" | "!=" | "<" | "<=" | ">=" | ">" | "~" | "!~")`
 	Value    Value  `@@`
 }
 
-func (f *Filter) String() string {
-	var sb strings.Builder
-	sb.Grow(256)
-	sb.WriteString("Terms:")
-	for _, p := range f.Terms {
-		sb.WriteString(fmt.Sprint(p))
-	}
-	return sb.String()
-}
-
 func (p *Term) String() string {
-	return fmt.Sprintf("\nKey is %v Value is %v Operator is %v", p.Key, p.Value, p.Operator)
+	return fmt.Sprintf("\n\t\tKey: %v\n\t\tOperator: %v\n\t\tValue: %v", p.Key, p.Operator, p.Value)
 }
 
 type Value struct {
-	String         string  `  @String`
+	String         string  `  @(String|Char|RawString)`
 	UnquotedString string  `| @Ident`
 	Float          float64 `| @Float`
 	Int            int     `| @Int`
